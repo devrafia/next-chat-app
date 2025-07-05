6f0339afc50f3d06193f923d23bbae14084d9126
Rafi Akmal <akmalrafi1132@gmail.com>
2025-07-05 11:03:42 +0700

    fix send message press enter

--- src/app/components/Chat/ChatInput.tsx
+++ src/app/components/Chat/ChatInput.tsx
@@ -9,7 +9,8 @@ import {
 import InputPrimary from "../Input/InputPrimary";
 
 export default function ChatInput({ roomId }: any) {
-  const sendMessage = async () => {
+  const sendMessage = async (e: any) => {
+    e.preventDefault();
     const input = document.getElementById("message-input") as HTMLInputElement;
     const text = input?.value;
     if (!text || !auth.currentUser) return;
@@ -33,30 +34,32 @@ export default function ChatInput({ roomId }: any) {
   };
   return (
     <>
-      <InputPrimary
-        type="text"
-        placeholder="Send a Message!"
-        id="message-input"
-      />
-      <div
-        onClick={sendMessage}
-        className="rounded-full bg-primary w-10 h-10 flex items-center justify-center cursor-pointer"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
+      <form onSubmit={(e) => sendMessage(e)} className="w-full flex gap-2">
+        <InputPrimary
+          type="text"
+          placeholder="Send a Message!"
+          id="message-input"
+        />
+        <button
+          type="submit"
+          className="rounded-full bg-primary w-10 h-10 flex items-center justify-center cursor-pointer"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-          />
-        </svg>
-      </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+            />
+          </svg>
+        </button>
+      </form>
     </>
   );
 }
