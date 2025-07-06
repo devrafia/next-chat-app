1d11b64cd0dbb3a377f91681b683ef91487681ac
Rafi Akmal <akmalrafi1132@gmail.com>
2025-07-06 14:36:17 +0700

    fix logout status user offline

--- src/app/components/Navbar/Logout.tsx
+++ src/app/components/Navbar/Logout.tsx
@@ -1,10 +1,18 @@
-import { auth } from "@/lib/firebase";
+import { auth, database } from "@/lib/firebase";
 import { signOut } from "firebase/auth";
+import { ref, set } from "firebase/database";
 
 export function Logout() {
   const handleLogout = async () => {
     try {
+      const uid = auth.currentUser.uid;
       await signOut(auth);
+      const userStatusDatabaseRef = ref(database, "status/" + uid);
+
+      set(userStatusDatabaseRef, {
+        state: "offline",
+        last_changed: Date.now(),
+      });
       console.log("Logout berhasil");
     } catch (error) {
       console.error("Logout gagal:", error);
